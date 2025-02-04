063d82d22ef8dbbfbaec2fbff770857b42383b76
M <mikki@nenna.is>
2025-02-04 01:48:23 +0100

    Blog added?
    * Updated some thingies in blog-like manner
    
    * mostly AI generated responsive stuff
    
    ---------
    
    Co-authored-by: mjkonko <m@nenna.is>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    {  path: 'blog', component: BlogComponent }
+];
