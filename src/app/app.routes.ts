eeb1cb0ba1335e57d621e1fcb6a97c5d6639cd16
M <mikki@nenna.is>
2025-09-13 08:55:14 +0200

    Fix route definition for BlogComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { BlogComponent } from './blog/blog.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    {  path: 'blog', component: BlogComponent }
+    { path: '', component: HomeComponent }
 ];
